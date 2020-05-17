eca02f8087d713734ffbbcbaa74919b02b6abf8a
Victor <victor_henrique@live.com>
2020-05-17 18:45:26 -0300

    Add: Footer

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,16 @@
 "use strict";
 
-console.log('teste');
+var btn = $('#top-btn');
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+btn.on('click', function (e) {
+  e.preventDefault();
+  $('html, body').animate({
+    scrollTop: 0
+  }, '300');
+});
--- resources/js/script.js
+++ resources/js/script.js
@@ -1,1 +1,14 @@
-console.log('teste');
+var btn = $('#top-btn');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
