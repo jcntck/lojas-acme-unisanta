0ebb7561bffebe77440be0b97fc0884efe8889c7
Victor Henrique <you@example.com>
2020-05-19 01:01:17 -0300

    Corrigindo as pastas de views

--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,27 @@ var router = express.Router();
 
 /* GET Página Inicial. */
 router.get("/", function (req, res, next) {
-  res.render("pages/index");
+  res.render("site/pages/index");
 });
 
 /* GET Sobre Nós. */
 router.get("/sobre-nos", function (req, res, next) {
-  res.render("pages/about");
+  res.render("site/pages/about");
 });
 
 /* GET Produtos. */
 router.get("/produtos", function (req, res, next) {
-  res.render("pages/products");
+  res.render("site/pages/products");
 });
 
 /* GET Lojas. */
 router.get("/lojas", function (req, res, next) {
-  res.render("pages/stores");
+  res.render("site/pages/stores");
 });
 
 /* GET Fale Conosco. */
 router.get("/fale-conosco", function (req, res, next) {
-  res.render("pages/contact");
+  res.render("site/pages/contact");
 });
 
 module.exports = router;
